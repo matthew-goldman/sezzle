af34743033dc1d2a1d4ced6623527ece0473fb68
matthew-goldman <matthew.x.goldman@gmail.com>
2026-02-06 13:04:53 -0500

    tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -54,9 +54,9 @@ module "vpc" {
 
 # Security Groups
 resource "aws_security_group" "alb" {
-  name_description = "${var.project_name}-alb-sg"
-  description      = "Security group for Application Load Balancer"
-  vpc_id           = module.vpc.vpc_id
+  name        = "${var.project_name}-alb-sg"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
